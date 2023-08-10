4036b21da2df60a81dba822d968ad4375ab89eee
Shivansh Goel <shivanshgoel009@gmail.com>
2023-08-10 13:39:22 +0530

    Fixed bugs

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
-from ast import Expression
-import pandas as pd
-
 from fastapi import FastAPI, Depends, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from sqlalchemy.orm import Session
 
+from typing import List
+
 import models
 from database import get_db
 import schemas
 
+import uuid
 
 app = FastAPI(prefix='/chatbot')
 origins = ['*']
 
-starting_id = "c8951605-3904-494f-a2a9-ce651dfb211b"
+# starting_id = "c8951605-3904-494f-a2a9-ce651dfb211b"
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,15 +25,23 @@ app.add_middleware(
 )
 
 
-@app.get("/get_questions", response_model=schemas.InitQuestion)
-def get_questions(db: Session = Depends(get_db)):
+def gen_session_id():
+    return uuid.uuid4()
+
+
+@app.get("/get_question", response_model=schemas.QuestionAnswers)
+def get_questions(payLoad: schemas.GetQuestion, db: Session = Depends(get_db)):
+    if payLoad.session_id is None:
+        payLoad.session_id = gen_session_id()
     question = db.query(models.Questions).filter(
-        models.Questions.id == starting_id).first()
-    print(str(question))
-    # return {"hello":"hey"}
-    return question
+        models.Questions.id == payLoad.id).first()
+    answers = db.query(models.Answers).filter(
+        models.Answers.question_id == payLoad.id).all()
+    response = {"session_id": payLoad.session_id,
+                "question": question, "answer_options": answers}
+    return response
 
 
-@app.post("/gen_response", status_code=status.HTTP_200_OK)
-def gen_response(payLoad: schemas.CellPosition):
+@app.post("/answer", status_code=status.HTTP_200_OK)
+def gen_response(payLoad: schemas.GetAnswer):
     return {"ok": "Ok"}
--- app/schemas.py
+++ app/schemas.py
@@ -1,24 +1,43 @@
 from pydantic import BaseModel, UUID4
 from uuid import uuid4
-from typing import List
+import uuid
+from typing import List, Optional, Union
 
 
-class CellPosition(BaseModel):
-    cell: str
+def gen_session_id():
+    return uuid.uuid4()
 
 
-class InitQuestion(BaseModel):
+class GetQuestion(BaseModel):
+    id: UUID4
+    session_id: Optional[UUID4] = None
+
+
+class GetAnswer(GetQuestion):
+    pass
+
+
+class QuestionResponse(BaseModel):
     id: UUID4
     expression: str
     keyword_intents: list
 
-    class Config:
-        orm_mode = True
+
+class AnswerOptions(BaseModel):
+    id: UUID4
+    expression: str
+    score: int
+    keyword_intents: List
+    suggested_actions: Optional[str] = None
+    elder_question_id: UUID4
+    progeny_question_id: Optional[UUID4] = None
+    question_id: UUID4
 
 
-class QuestionResponse(BaseModel):
-    question: str
-    options: dict
+class QuestionAnswers(BaseModel):
+    session_id: UUID4
+    question: QuestionResponse
+    answer_options: List[AnswerOptions]
 
     class Config:
         orm_mode = True
