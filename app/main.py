a69cb1cb79ee16c974cbe86f65f2542d54dfa523
Shivansh Goel <shivanshgoel009@gmail.com>
2023-08-17 11:30:48 +0530

    Fixed bugs

--- app/main.py
+++ app/main.py
@@ -211,13 +211,13 @@ def gpt_response(payLoad: schemas.GPTQuery, db: Session = Depends(get_db)):
 
 @app.get("/translate_database")
 def translate_database(db: Session = Depends(get_db)):
-    # questions = db.query(models.Questions).all()
-    # for question in questions:
-    #     question_dict = question.__dict__
-    #     question_dict["expression"] = translate_text(question_dict["expression"])
-    #     del question_dict["_sa_instance_state"]
-    #     new_question = models.HindiQuestions(**question_dict)
-    #     db.add(new_question)
+    questions = db.query(models.Questions).all()
+    for question in questions:
+        question_dict = question.__dict__
+        question_dict["expression"] = translate_text(question_dict["expression"])
+        del question_dict["_sa_instance_state"]
+        new_question = models.HindiQuestions(**question_dict)
+        db.add(new_question)
 
     answers = db.query(models.Answers).all()
     for answer in answers:
