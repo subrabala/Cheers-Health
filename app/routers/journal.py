9462c7e10774655d1abacb8fe694cc5c80b1649c
Shivansh Goel <shivanshgoel009@gmail.com>
2023-08-18 12:19:52 +0530

    Fixed Bugs in journal/get

--- app/routers/journal.py
+++ app/routers/journal.py
@@ -21,13 +21,18 @@ router = APIRouter(
 @router.get("/get/{user_id}/{journal_id}", response_model=List[schemas.JournalDetails])
 def get_journals(user_id: UUID4, journal_id: UUID4, db: Session = Depends(get_db)):
     journals = db.query(models.Journal).filter(
-        models.Journal.journal_id == journal_id, models.Journal.user_id == user_id).all()
+        models.Journal.journal_id == journal_id, models.Journal.user_id == user_id).order_by(models.Journal.answered_at).all()
     return journals
 
 
 @router.get("/get/{user_id}", response_model=List[schemas.UserJournals])
 def get_journal_ids(user_id: UUID4, db: Session = Depends(get_db)):
-    journal_ids = db.query(models.Journal.answered_at, models.Journal.journal_id).filter(
+    journal_ids = db.query(models.Journal.journal_id).distinct().filter(
         models.Journal.user_id == user_id).all()
-    print(journal_ids)
-    return journal_ids
+    response = []
+    for journal_id in journal_ids:
+        answered_at = db.query(models.Journal.answered_at).filter(
+            models.Journal.journal_id == journal_id[0]).first()
+        response.append({"answered_at":answered_at[0], "journal_id":journal_id[0]})
+        print(response)
+    return response
