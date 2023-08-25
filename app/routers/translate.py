6e0bc299487eda11ca98244866210cd652cf3cb3
Shivansh Goel <shivanshgoel009@gmail.com>
2023-08-25 18:59:20 +0530

    Added paraphrasing layer to translation api

--- app/routers/translate.py
+++ app/routers/translate.py
@@ -14,25 +14,38 @@ router = APIRouter(
 
 @router.get("/database")
 def translate_database(db: Session = Depends(get_db)):
+    delete_logs = db.query(models.HindiQuestions)
+    delete_logs.delete(synchronize_session=False)
+
+    delete_logs = db.query(models.HindiAnswers)
+    delete_logs.delete(synchronize_session=False)
+    db.commit()
+
     questions = db.query(models.Questions).all()
     for question in questions:
         question_dict = question.__dict__
         question_dict["expression"] = translate_text(
-            question_dict["expression"])
+            question_dict["expression"], "q")
         del question_dict["_sa_instance_state"]
+        print(question_dict["expression"])
         new_question = models.HindiQuestions(**question_dict)
         db.add(new_question)
+        # db.commit()
 
     answers = db.query(models.Answers).all()
     for answer in answers:
         answer_dict = answer.__dict__
-        answer_dict["expression"] = translate_text(answer_dict["expression"])
+        answer_dict["expression"] = translate_text(
+            answer_dict["expression"], "a")
+        print(answer_dict["expression"])
         if answer_dict["suggested_action"] is not None:
             answer_dict["suggested_action"] = translate_text(
-                answer_dict["suggested_action"])
+                answer_dict["suggested_action"], "s")
+            print(answer_dict["suggested_action"])
         del answer_dict["_sa_instance_state"]
         new_answer = models.HindiAnswers(**answer_dict)
         db.add(new_answer)
+        # db.commit()
     db.commit()
 
     return {"Details": "Database Translated"}
--- app/utils.py
+++ app/utils.py
@@ -1,21 +1,46 @@
 import uuid
 from google.cloud import translate
+import openai
+
+from config import settings
+
+openai.api_key = settings.openai_api_key
 
 
 def gen_uuid():
     return uuid.uuid4()
 
 
-def translate_text(text):
+def translate_text(text, type):
     text = str(text)
+    if type == "q":
+        paraphrased_text = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": f"Paraphrase the following text to make it sound like you are asking a question: {text}"}]
+        )
+    elif type == "a":
+        paraphrased_text = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": f"Paraphrase the following text to make it sound like you are giving an answer to an asked question:: {text}"}]
+        )
+    elif type == "s":
+        paraphrased_text = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": f"Paraphrase the following text to make it sound like you are suggesting an action: {text}"}]
+        )
+    # else:
+    #     paraphrased_text = text
     project_id="cheers-wisdom-translate"
-    client = translate.TranslationServiceClient()
-    location = "global"
-    parent = f"projects/{project_id}/locations/{location}"
-    response = client.translate_text(
+    client=translate.TranslationServiceClient()
+    location="global"
+    parent=f"projects/{project_id}/locations/{location}"
+    response=client.translate_text(
         request={
             "parent": parent,
-            "contents": [text],
+            "contents": [str(paraphrased_text.choices[0].message.content.strip())],
             "mime_type": "text/plain",
             "source_language_code": "en-US",
             "target_language_code": "hi",
